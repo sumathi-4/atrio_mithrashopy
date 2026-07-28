a2d9c0bce8aadfd340f63471698bee2655a03581
sumathi-4 <sumathisaraswathi4@gmail.com>
2026-07-28 11:26:21 +0530

    allow fallback port in user vite config

--- user/vite.config.js
+++ user/vite.config.js
@@ -33,7 +33,7 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    strictPort: true,
+    strictPort: false,
     proxy: {
       // Proxy all /api requests to the backend during development
       '/api': {
